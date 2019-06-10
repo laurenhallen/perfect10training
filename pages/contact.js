16aa04adce82c2db569a070e27390fb6ec3a6635
Lauren Hollie Allen <lauren_hollie@me.com>
2019-06-10 10:50:39 -0400

    updates to code

--- pages/contact.js
+++ pages/contact.js
@@ -4,36 +4,37 @@ function Contact() {
     return (
       
         <div className="container contact">
-	<div className="row">
-		<div className="col-md-3">
-			<div className="contact-info">
-				<img src="https://image.ibb.co/kUASdV/contact-image.png" alt="image"/>
-				<h2>Contact Me</h2>
-				<h4>Let's start YOUR fitness journey together!</h4>
-			</div>
-		</div>
-		<div className="col-md-9">
-			<div className="contact-form">
+        <div className="row">
+            <div className="col-md-3">
+                <div className="contact-info">
+                    <img src="https://image.ibb.co/kUASdV/contact-image.png" alt="image"/>
+                    <h2>Contact Me</h2>
+                    <h4>Let's start YOUR fitness journey together!</h4>
+                </div>
+            </div>
+            <div className="col-md-9">
+			<div className="contact-form" action="contactform.php" method="post">
 				<div className="form-group">
-				  <label className="control-label col-sm-2" for="fname">First Name:</label>
-				  <div className="col-sm-10">          
-					<input type="text" className="form-control" id="fname" placeholder="Enter First Name" name="fname"></input>
+				  <label className="control-label col-sm-2" for="name">Full Name:</label>
+				  <div className="col-sm-10">       
+					<input type="text" className="form-control" id="name" placeholder="Enter Full Name" name="name" ></input>
 				  </div>
 				</div>
-				<div className="form-group">
-				  <label className="control-label col-sm-2" for="lname">Last Name:</label>
-				  <div className="col-sm-10">          
-					<input type="text" className="form-control" id="lname" placeholder="Enter Last Name" name="lname"></input>
-				  </div>
-				</div>
+				
 				<div className="form-group">
 				  <label className="control-label col-sm-2" for="email">Email:</label>
 				  <div className="col-sm-10">
-					<input type="email" className="form-control" id="email" placeholder="Enter email" name="email"></input>
+					<input type="email" className="form-control" id="email" placeholder="Enter Email" name="email" ></input>
+				  </div>
+				</div>
+                <div className="form-group">
+				  <label className="control-label col-sm-2" for="trainer">Trainer's Name:</label>
+				  <div className="col-sm-10">
+					<input type="text" className="form-control" id="trainer" placeholder="Preferred Trainer" name="trainer" ></input>
 				  </div>
 				</div>
 				<div className="form-group">
-				  <label className="control-label col-sm-2" for="comment">Comment:</label>
+				  <label className="control-label col-sm-2" for="comment">Tell me why you are interested:</label>
 				  <div className="col-sm-10">
 					<textarea className="form-control" rows="5" id="comment"></textarea>
 				  </div>
@@ -45,7 +46,7 @@ function Contact() {
 				</div>
 			</div>
 		</div>
-	</div>
+	</div>  
 </div>
 
     )
